32c9542775b069a069a2bca99a08d05745fb7c58
Aliskhan <aliskhanreventon@gmail.com>
2025-07-23 15:25:40 +0300

    Правка l1.5: безопасное завершение горутины через done

--- tasks/task-5/main.go
+++ tasks/task-5/main.go
@@ -12,16 +12,21 @@ import (
 
 func main() {
 	ch := make(chan int)
-
+	done := make(chan struct{}) // channel for signal goroutine to finish
 	// Timeout channel to stop the program after 5 seconds
 	timeout := time.After(5 * time.Second)
 
 	go func() {
 		i := 1
 		for {
-			ch <- i * i // Send square of i to the channel
-			i++
-			time.Sleep(1 * time.Second) // Simulate some work
+			select {
+			case <-done:
+				fmt.Println("Stopping the goroutine.")
+				return
+			case ch <- i * i: // Send square of i to the channel
+				i++
+				time.Sleep(1 * time.Second) // Simulate some work
+			}
 		}
 	}()
 
@@ -30,7 +35,10 @@ func main() {
 		case val := <-ch:
 			fmt.Println("Received:", val)
 		case <-timeout:
-			fmt.Println("Timeout reached, stopping the program.")
+			fmt.Println("Timeout reached.")
+			fmt.Println("Stopping the program.")
+			close(done)
+			time.Sleep(100 * time.Millisecond) // Wait for goroutine to finish
 			return
 		}
 	}
